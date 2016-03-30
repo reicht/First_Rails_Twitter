d233e079e51a5ca3ee4e88cea5e0c15f77c350b1
Jordan Corley <rainwolfj@gmail.com>
2016-03-30 15:27:12 -0500

    Users seedable and functional, adding chirps layer next

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render template: '/users/index.html.erb'
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+rand(10..100).times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
