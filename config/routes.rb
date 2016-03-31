4eb1d88c78f1673e3a350d3ace203e69285d30da
Jordan Corley <rainwolfj@gmail.com>
2016-03-31 07:25:28 -0500

    Morning commit, not sure what was left uncommitted, oopsy boopsy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get '' => 'users#index'
-
   get 'tweets' => 'tweets#index'
 
   get 'users' => 'users#index'
@@ -24,7 +22,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'tweets#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
